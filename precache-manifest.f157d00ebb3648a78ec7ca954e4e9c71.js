985145c16606158d5cbdacd8e1264ed1f713fe1f
Saurav <saurav.pandey@cloudyfox.com>
2020-05-21 12:01:44 +0545

    Updates

---
+++ precache-manifest.f157d00ebb3648a78ec7ca954e4e9c71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f103253e5505a9d102f3da7079598d8",
+    "url": "/open-data-portal/index.html"
+  },
+  {
+    "revision": "731f3df67fad15f3e5db",
+    "url": "/open-data-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbb241f7775843b71349",
+    "url": "/open-data-portal/static/js/2.41398a92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/open-data-portal/static/js/2.41398a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731f3df67fad15f3e5db",
+    "url": "/open-data-portal/static/js/main.43f31e8d.chunk.js"
+  },
+  {
+    "revision": "922a34b1e6e1223acd3e",
+    "url": "/open-data-portal/static/js/runtime-main.28be7ba9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/open-data-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
